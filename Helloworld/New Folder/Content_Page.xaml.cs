82cbfbaa02cdd906de769ad8be5591a09d230a02
Padalingam <padalingam@nalashaa.com>
2017-08-02 11:16:54 +0530

    File Modified

--- Helloworld/New Folder/Content_Page.xaml.cs
+++ Helloworld/New Folder/Content_Page.xaml.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Linq;
+using Xamarin.Auth;
 using Xamarin.Forms;
 
 namespace Helloworld
 {
 	public partial class Content_Page : ContentPage
 	{
+		public string UserName
+		{
+			get
+			{
+				var account = AccountStore.Create().FindAccountsForService("Helloworld").FirstOrDefault();
+				return (account != null) ? account.Username : null;
+			}
+		}
+
+		public string Password
+		{
+			get
+			{
+				var account = AccountStore.Create().FindAccountsForService("Helloworld").FirstOrDefault();
+				return (account != null) ? account.Properties["Password"] : null;
+			}
+		}
+
 		public Content_Page()
 		{
 			InitializeComponent();
@@ -15,11 +34,12 @@ namespace Helloworld
 
 		void Handle_Clicked(object sender, System.EventArgs e)
 		{
-			
-			Navigation.PushAsync(new SamplePageOne());
+			SaveCredentials("Mani", "Hello");
+			System.Diagnostics.Debug.WriteLine("Username is {0} and password is {1}", UserName, Password);
+			//Navigation.PushAsync(new SamplePageOne());
 		}
 
-	   	protected override void OnDisappearing()
+		protected override void OnDisappearing()
 		{
 			base.OnDisappearing();
 		}
@@ -31,7 +51,20 @@ namespace Helloworld
 
 		void Handle_ClickedOne(object sender, System.EventArgs e)
 		{
-			throw new NotImplementedException();
+
+		}
+
+		public void SaveCredentials(string userName, string password)
+		{
+			if (!string.IsNullOrWhiteSpace(userName) && !string.IsNullOrWhiteSpace(password))
+			{
+				Account account = new Account
+				{
+					Username = userName
+				};
+				account.Properties.Add("Password", password);
+				AccountStore.Create().Save(account, "Helloworld");
+			}
 		}
 	}
 }
